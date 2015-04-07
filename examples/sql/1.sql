4503842677987d426e426adf5da09ebffca2e4aa
Mantas Markevicius <mantasmarkeviciuslt@gmail.com>
2015-04-07 11:44:33 +0100

    removes deleted files

--- examples/sql/1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE people(
-	name text,
-	age int
-);
-
-CREATE TABLE couples(
-	her text,
-	him text
-);
--- examples/sql/3.sql
+++
@@ -1,3 +0,0 @@
-select w.name as name, w.age - m.age as diff
-from couples as c, people as w, people as m
-where c.her = w.name and c.him = m.name and w.age > m.age;
