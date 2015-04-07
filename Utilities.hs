d49d0bd3739b5b99077435ba387a50b5c6f0b896
Mantas Markevicius <mantasmarkeviciuslt@gmail.com>
2015-04-07 11:43:43 +0100

    add helper function for converting results to record to Utilities

--- Utilities.hs
+++ Utilities.hs
@@ -3,6 +3,8 @@ module Utilities where
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Data.Char (toUpper, toLower)
+import Database.HDBC
+import Data.Convertible
 
 liftLinq  a = unsafeTExpCoerce $ lift a
 
@@ -13,3 +15,33 @@ toTitleCase [] = ""
 toTitleCase [x] = [toUpper x]
 toTitleCase (x:xs) = toUpper x : xs
 
+toRecord toRec con valsIO  = do
+	vals <- valsIO
+	let conv = toRec con vals
+	return conv
+
+toRec con vals = map (\x -> con (fromSql $ x!!0)) vals
+toRec2 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1)) vals
+toRec3 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2)) vals
+toRec4 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3)) vals
+toRec5 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4)) vals
+toRec6 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4) (fromSql $ x!!5)) vals
+toRec7 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4) (fromSql $ x!!5) (fromSql $ x!!6)) vals
+toRec8 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4) (fromSql $ x!!5) (fromSql $ x!!6) (fromSql $ x!!7)) vals
+toRec9 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4) (fromSql $ x!!5) (fromSql $ x!!6) (fromSql $ x!!7) (fromSql $ x!!8)) vals
+toRec10 con vals = map (\x -> con (fromSql $ x!!0) (fromSql $ x!!1) (fromSql $ x!!2) (fromSql $ x!!3) (fromSql $ x!!4) (fromSql $ x!!5) (fromSql $ x!!6) (fromSql $ x!!7) (fromSql $ x!!8) (fromSql $ x!!9)) vals
+
+toTuple toTup valsIO  = do
+	vals <- valsIO
+	let conv = toTup vals
+	return conv
+
+toTup2 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1)) vals
+toTup3 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2)) vals
+toTup4 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3)) vals
+toTup5 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4)) vals
+toTup6 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4, fromSql $ x!!5)) vals
+toTup7 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4, fromSql $ x!!5, fromSql $ x!!6)) vals
+toTup8 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4, fromSql $ x!!5, fromSql $ x!!6, fromSql $ x!!7)) vals
+toTup9 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4, fromSql $ x!!5, fromSql $ x!!6, fromSql $ x!!7, fromSql $ x!!8)) vals
+toTup10 vals = map (\x -> (fromSql $ x!!0, fromSql $ x!!1, fromSql $ x!!2, fromSql $ x!!3, fromSql $ x!!4, fromSql $ x!!5, fromSql $ x!!6, fromSql $ x!!7, fromSql $ x!!8, fromSql $ x!!9)) vals
